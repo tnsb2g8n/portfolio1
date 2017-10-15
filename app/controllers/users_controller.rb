3f86afd4f8e073e8a0c638b3ca5a2962a441c6fc
Yuya Nakano <tns.b2g8n@gmail.com>
2017-10-15 06:04:55 +0000

    rails10.3.4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :correct_user,   only: [:edit, :update]  
+  
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+  
   def show
     @user = User.find(params[:id])
   end
@@ -11,10 +18,24 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       log_in @user
-      flash[:success] = "Welcome to the Sample App!"      
+      flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
       render 'new'
+    end
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
     end
   end  
   
@@ -25,4 +46,20 @@ class UsersController < ApplicationController
                                    :password_confirmation)
     end
   
+    # beforeアクション
+
+    # ログイン済みユーザーかどうか確認
+    def logged_in_user
+      unless logged_in?
+        store_location    
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+    
+    # 正しいユーザーかどうか確認
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end    
 end
