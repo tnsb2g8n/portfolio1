4d0f0c980441e3de336c9ef61bf629133ed1f352
Yuya Nakano <tns.b2g8n@gmail.com>
2017-10-15 22:13:35 +0000

    r12.3

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   get 'sessions/new'
 
   get 'users/new'
@@ -12,5 +16,6 @@ Rails.application.routes.draw do
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
   resources :users
-  resources :account_activations, only: [:edit]  
+  resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]  
 end
