a256552a7126afe2243cf0a5942c5075b3bb8857
tnsb2g8n <tns.b2g8n@gmail.com>
2017-10-23 12:16:53 +0000

    14.27

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -14,7 +14,6 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   
   test "should get new" do
     get signup_path    
-    get users_new_url
     assert_response :success
   end
 
@@ -61,4 +60,13 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to root_url
   end
   
+  test "should redirect following when not logged in" do
+    get following_user_path(@user)
+    assert_redirected_to login_url
+  end
+
+  test "should redirect followers when not logged in" do
+    get followers_user_path(@user)
+    assert_redirected_to login_url
+  end  
 end
