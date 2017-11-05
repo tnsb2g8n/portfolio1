00f2d6c2ae13af69d02e5acc6296a51684f1cc4b
tnsb2g8n <tns.b2g8n@gmail.com>
2017-11-05 07:02:57 +0000

    gogo1005

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -9,20 +9,11 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get help" do
-    get help_path
-    assert_response :success
-    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get about" do
-    get about_path
-    assert_response :success
-    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get contact" do
-    get contact_path
-    assert_response :success
-    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
   end
 end
